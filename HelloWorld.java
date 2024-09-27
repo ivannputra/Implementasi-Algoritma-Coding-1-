c97db2a1bb4128846008974b5dd10605705cdc21
ivannputra <ivanmahardika805@gmail.com>
2024-09-27 11:29:01 +0700

    Tambah Nama

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 public class HelloWorld{
     public static void main(String[] args){
-        System.out.printlng("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Nama saya");
+        System.out.println("Ivan Putra Maharadika");
+        
         
     }
 }
